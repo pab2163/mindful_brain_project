d99738b5854ecdafc49acdecdb73bee7aa692932
Paul Alexander Bloom <paul.bloom@nyspi.columbia.edu>
2022-11-04 14:07:26 -0400

    fix keys to 2-3

--- reMIND_selfref.py
+++ reMIND_selfref.py
@@ -270,7 +270,7 @@ def run_trial(trial_type, fixation_duration, practice=False):
     while continueRoutine:            
         trial_time = trial_clock.getTime()
         if trial_time > 0 and trial_time < trial_duration:
-            theseKeys = event.getKeys(keyList=['1', '2', 'escape'])
+            theseKeys = event.getKeys(keyList=['2', '3', 'escape'])
             if "escape" in theseKeys:
                 endExpNow = True
 
@@ -281,10 +281,10 @@ def run_trial(trial_type, fixation_duration, practice=False):
                                     expName, expInfo['frameRate'], time.time(), triggerClock.getTime(), 'response', 1, 
                                     trial_word, trial_clock.getTime(), theseKeys[0]])
                 # change color of selected word
-                if '1' in theseKeys:
+                if '2' in theseKeys:
                     no.bold = True
                     no.italic = True
-                elif '2' in theseKeys:
+                elif '3' in theseKeys:
                     yes.bold = True
                     yes.italic = True
                 word.draw()
@@ -319,12 +319,12 @@ def run_practice():
         \n\nPlease press your index finger to answer NO')
     instruct_text.draw()
     win.flip()
-    wait_for_keypress(key_list=['1'])
+    wait_for_keypress(key_list=['2'])
     instruct_text.setText('Just to make sure everything is working with the buttons.\
         \n\nPlease press your middle finger to answer YES')
     instruct_text.draw()
     win.flip()
-    wait_for_keypress(key_list=['2'])
+    wait_for_keypress(key_list=['3'])
     instruct_text.setText('Great! We will go through a few practice trials of each type now.\
         \n\nTry to make your decision quickly!')
     instruct_text.draw()
