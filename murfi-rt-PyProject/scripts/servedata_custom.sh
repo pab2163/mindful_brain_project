4b0b6e3417844a832bf7b5e6c3e2e565afa2a978
auerbachLinux <auerbachlinux@system76-pc.localdomain>
2022-12-05 10:23:27 -0500

    servedata_custom.sh for selecting nii volumes for simulation

--- murfi-rt-PyProject/scripts/servedata_custom.sh
+++ murfi-rt-PyProject/scripts/servedata_custom.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
+# Clemens Bauer
+# Modified by Paul Bloom November 2022
+
+# Serves nii volumes already on the computer's hard drive to MURFI (for simulating runs)
 
 
+## ARGUMENTS ##
+
+# TR (in ms)
+tr=$1
+
+# MNumber of volumes to serve
+volumes=$2
+
+# Path to directory with serve in simulation
+path=$3
+
+# File stem (an integer) indicating the number of the run that should be served
+runstem=$4 
 
 if [ "$1" == "--help" ]; then
     echo "$0 [TR(ms)] [volumes] [path_spec]"
     exit 0
 fi
 
-tr=$1
-volumes=$2
-path=$3
 port=15000
 host='127.0.0.1'
 sleep=1.2
@@ -25,18 +39,6 @@ if [ x"$servepath" == x ]; then
 fi
 
 
-servenii ../subjects/sub-R61MBNFD999/img/img 4 ${volumes} 1 68 $tr $port $host
+servenii ${path}/img $runstem ${volumes} 1 68 $tr $port $host
 
-
-# if [ "$1" == "250vol" ]; then
-#     servenii img/img 1 250 1 68 $tr $port $host
-# fi
-
-# if [ "$1" == "2vol" ]; then
-#     servenii img/img 1 2 1 68 $tr $port $host
-# fi
-
-# if [ "$1" == "120vol" ]; then
-#     servenii img/img 1 120 1 68 $tr $port $host
-# fi
 
