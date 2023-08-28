e4dcc964514d08d379fdef2f01aed3dfffc25cec
Paul A. Bloom <pab2163@columbia.edu>
2023-08-28 13:25:51 -0400

    updates to sret power analysis

--- self_reference/power_analysis/power_analysis.R
+++ self_reference/power_analysis/power_analysis.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(simr)
-iter = 1000
+library(EMAtools)
+iter = 100
 # 2 timepoints: pre/post
 time = 0:1
 
@@ -13,7 +14,6 @@ group = c('15', '30')
 # 90 participants
 df = expand.grid(id = 1:45, time =time, run = run, group = group)
 df = mutate(df, id = ifelse(group == '15', id + 45, id))
-
 
 # create 15% attrition (MCAR)
 df_post=df %>%
@@ -37,7 +37,8 @@ res = 1
 model_dose <- makeLmer(y ~ group*time + (time|id), fixef=fixed_dose, VarCorr=rand_dose, sigma=res, data=df)
 model_main_effect <- makeLmer(y ~ time + (time|id), fixef=fixed_main, VarCorr=rand_main, sigma=res, data=df)
 
-
+n_tests = 3
+alpha_level = .05/n_tests
 
 # Dose --------------------------------------------------------------------
 
@@ -52,7 +53,7 @@ for (effect_size in effect_sizes){
   eff_df_dose$omega2[eff_df_dose$effect_sizes == effect_size] = effsize_m$Omega2_partial[effsize_m$Parameter =='group:time']
 
   sim_treat <- powerSim(model_dose, nsim=iter, test = fixed(xname='group30:time', 't'))
-  eff_df_dose$power[eff_df_dose$effect_sizes == effect_size] = sim_treat$x
+  eff_df_dose$power[eff_df_dose$effect_sizes == effect_size] = sum(sim_treat$pval < alpha_level)
   
 }
 
@@ -78,7 +79,7 @@ for (effect_size in effect_sizes){
   eff_df_main$omega2[eff_df_main$effect_sizes == effect_size] = effsize_m$Omega2_partial[effsize_m$Parameter =='time']
   
   sim_treat <- powerSim(model_main_effect, nsim=iter, test = fixed(xname='time', 't'))
-  eff_df_main$power[eff_df_main$effect_sizes == effect_size] = sim_treat$x
+  eff_df_main$power[eff_df_main$effect_sizes == effect_size] = sum(sim_treat$pval < alpha_level)
   
 }
 
@@ -99,8 +100,9 @@ eff_df_main$type = 'Time Main Effect'
 
 eff_df = rbind(eff_df_dose, eff_df_main)
 
-p = ggplot(data = eff_df, aes(x = omega2, y = power/10, color = type)) +
-  geom_hline(yintercept = 80, lty = 2) +
+p = ggplot(data = eff_df, aes(x = omega2, y = power/1000, color = type)) +
+  geom_point() + 
+  geom_hline(yintercept = .8, lty = 2) +
   geom_vline(xintercept = 0.06, lty = 2) +
   geom_vline(xintercept = 0.14, lty = 2) +
   geom_line() +
@@ -108,6 +110,23 @@ p = ggplot(data = eff_df, aes(x = omega2, y = power/10, color = type)) +
   xlim(0, 0.3)
 
 
-write.csv(eff_df, file = 'Downloads/effectsize_df.csv')
-ggsave(p, file = 'Downloads/effectsizeplot.png')
- 
+write.csv(eff_df, file = 'effectsize_df.csv')
+ggsave(p, file = 'effectsizeplot.png')
+
+p
+
+
+p1 = ggplot(data = eff_df, aes(x = -1*effect_sizes, y = power, color = type)) +
+  geom_point() + 
+  geom_hline(yintercept = 80, lty = 2) +
+  geom_vline(xintercept = 0.2, lty = 2) +
+  geom_vline(xintercept = 0.5, lty = 2) +
+  geom_vline(xintercept = 0.8, lty = 2) +
+  geom_line() +
+  labs(x = "Effect Size (Cohen's D)")
+
+
+p1
+
+sim_treat$x
+
