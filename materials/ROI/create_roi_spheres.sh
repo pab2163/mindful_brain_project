fe9721e272f3be3b4db26d0f4d7fd2308a3379c0
Paul A. Bloom <pab2163@columbia.edu>
2023-09-06 15:18:44 -0400

    create 3rd sphere

--- materials/ROI/create_roi_spheres.sh
+++ materials/ROI/create_roi_spheres.sh
@@ -4,9 +4,14 @@
 # MNI coordinates 2, -60, 38
 3dcalc -a ../../murfi-rt-PyProject/scripts/MNI152_T1_2mm_brain.nii.gz  -expr 'step(64-(x+2)*(x+2)-(y-60)*(y-60)-(z-38)*(z-38))' -prefix pcc_sphere_8mm
 
+# MNI coordinates 2, -60, 36
+3dcalc -a ../../murfi-rt-PyProject/scripts/MNI152_T1_2mm_brain.nii.gz  -expr 'step(64-(x+2)*(x+2)-(y-60)*(y-60)-(z-36)*(z-36))' -prefix pcc_sphere_cog_8mm
+
 # Convert to .nii
 3dAFNItoNIFTI mpfc_sphere_8mm+tlrc.BRIK -prefix mpfc_sphere_8mm.nii
 3dAFNItoNIFTI pcc_sphere_8mm+tlrc.BRIK -prefix pcc_sphere_8mm.nii
+3dAFNItoNIFTI pcc_sphere_cog_8mm+tlrc.BRIK -prefix pcc_sphere_cog_8mm.nii
+
 
 rm *.BRIK
 rm *.HEAD
