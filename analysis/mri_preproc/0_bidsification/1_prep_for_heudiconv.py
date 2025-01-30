e0841f3c61dd0078cecc458553a376df91e833dd
Mexico <mexico@Mexicos-MacBook-Air.local>
2025-01-30 10:43:26 -0500

    pipeline for removing runs from heudiconv process

--- analysis/mri_preproc/0_bidsification/1_prep_for_heudiconv.py
+++ analysis/mri_preproc/0_bidsification/1_prep_for_heudiconv.py
@@ -8,10 +8,27 @@ def organize_dicoms(main_path, dicom_out_path, subject):
     Organizes dicoms pre-heudiconv for one subject
     '''
     
-    sub_out_path = f'{dicom_out_path}/{subject}/'
-    os.system(f'mkdir {sub_out_path}')
-    os.system(f'mkdir {sub_out_path}/loc')
-    os.system(f'mkdir {sub_out_path}/nf')
+    # find out if file specifying exclusions exists
+    exclude_runlist_path=f'bids_ignore_files/{subject}.txt'
+    
+    if os.path.isfile(exclude_runlist_path):
+        runs_to_exclude=True
+        with open(exclude_runlist_path, 'r') as file:
+            bids_ignore_runs = file.readlines()
+
+        # Remove trailing newline characters from each line
+        bids_ignore_runs = [line.rstrip('\n') for line in bids_ignore_runs]
+        print(bids_ignore_runs)
+
+    else:
+        print(f'No file exists marking runs to be ignored for {subject} - is this correct?')
+        runs_to_exclude=False
+
+    # sub_out_path = f'{dicom_out_path}/{subject}/'
+    # os.system(f'mkdir {sub_out_path}')
+    # os.system(f'mkdir {sub_out_path}/loc')
+    # os.system(f'mkdir {sub_out_path}/nf')
+
 
     # For CU data (subject id remind2###) - need to unzip dicoms
     if 'remind2' in subject:
@@ -20,23 +37,38 @@ def organize_dicoms(main_path, dicom_out_path, subject):
             print(label)
             if label.endswith('Auerbach^REMIND') or label.endswith('loc'):
                 session = 'loc'
+                os.system(f"mv {label} {label.replace('Auerbach^REMIND', 'loc')}")
+
             elif label.endswith('Auerbach^REMIND_1') or label.endswith('nf'): 
                 session = 'nf'
-    
+                os.system(f"mv {label} {label.replace('Auerbach^REMIND_1', 'nf')}")
+
+            # exclude marked runs
+            if runs_to_exclude:
+                exclude_runs(bids_ignore_runs, subject)
+
             # find all the zipped dicom folders, unzip them to new directory
             zip_dicom_list = list(Path(label).rglob("*dicom.[z][i][p]"))
             unzip_dicoms(zip_dicom_list, sub_out_path, session)
 
-    # For NEU data, folders are separated by session. Nest them
-    if 'remind3' in subject:
-        pass
+    # # For NEU data, folders are separated by session. Nest them
+    # if 'remind3' in subject:
+    #     pass
 
-def exclude_runs(run_list):
+def exclude_runs(run_list, subject):
     '''
     Given a list of "bad" runs to exclude - make sure these are not in the file structure to be passed to heudiconv
 
     '''
-    pass
+    base_path = f'{main_path}/cu/{subject}'
+    for run in run_list:
+        run = run.replace('ses-nf', 'nf')
+        run = run.replace('ses-loc', 'loc')
+        run_path = f'{base_path}/{run}'
+        print(run_path)
+
+        # remove the excluded runs
+        #os.system(f'rm -rf {run_path}')
 
 def unzip_dicoms(dicom_list, sub_out_path, session):
     '''
