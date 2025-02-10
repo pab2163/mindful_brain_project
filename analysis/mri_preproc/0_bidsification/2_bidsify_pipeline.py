3baccb35f1e42c65f06e8372b5d1d5d006b1666d
Mexico <mexico@Mexicos-MacBook-Air.local>
2025-02-09 23:51:42 -0500

    updates to bidsify pipeline

--- analysis/mri_preproc/0_bidsification/2_bidsify_pipeline.py
+++ analysis/mri_preproc/0_bidsification/2_bidsify_pipeline.py
@@ -1,12 +1,29 @@
+import logging
+from datetime import datetime
 import os
 import re
 import glob
 
+# Configure logging
+log_filename = f"bidsify_logs/bidsify_log_{datetime.now().strftime('%Y-%m-%d')}.txt"
+logging.basicConfig(filename=log_filename, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def log_and_print(message, level="info"):
+    print(message)
+    if level == "info":
+        logging.info(message)
+    elif level == "warning":
+        logging.warning(message)
+    elif level == "error":
+        logging.error(message)
+
+
+
 pattern = re.compile(r"remind\d{4}")
 
 # paths
-cu_path = '/Volumes/columbia/mbNF_MDD/DATA/MRI/flywheel_dicom/flywheel/auerbach/REMIND/*'
-neu_path = '/Volumes/columbia/mbNF_MDD/DATA/MRI/from_neu/sourcedata/*'
+cu_path = '/neurodata/mindful_brain_project/data/dicom_raw/cu/*'
+neu_path = '/neurodata/mindful_brain_project/data/dicom_raw/neu/*'
 
 
 # get cu ids
@@ -23,15 +40,15 @@ unique_remind_ids_neu = list({match.group() for folder in neu_folders if (match 
 unique_remind_ids.extend(unique_remind_ids_neu)
 
 # Print ids
-print(sorted(unique_remind_ids))
-print(f'Total of {len(unique_remind_ids)} participants found')
+log_and_print(sorted(unique_remind_ids))
+log_and_print(f'Total of {len(unique_remind_ids)} participants found')
 
 # paths to prepped dicoms/bids data
-prepped_dicom_path='/neurodata/mindful_brain_project/data/dicom_preppred_for_heudiconv'
+prepped_dicom_path='/neurodata/mindful_brain_project/data/dicom_prepped_for_heudiconv'
 bids_path = '/neurodata/mindful_brain_project/data/bids_data'
 
-# loop through ids to prep dicoms / bidsfy
-for id in unique_remind_ids:
+def run_pipeline_one_participant(id):
+	log_and_print(f'\n{id}')
 	'''
 	run dicom prep only if output prepped directory doesn't exist
 	this step 
@@ -43,10 +60,11 @@ for id in unique_remind_ids:
 
 	'''
 	prep_command=f'python 1_prep_for_heudiconv.py {id}'
-	if os.path.isdir(f'prepped_dicom_path/{id}'):
-		print(f'Dicom prep already exists for {id}')
+	if os.path.isdir(f'{prepped_dicom_path}/{id}'):
+		log_and_print(f'Dicom prep already exists for {id}')
 	else:
-		print(prep_command)
+		log_and_print(f'Prepping dicoms for {id}')
+		os.system(prep_command)
 
 
 	'''
@@ -58,17 +76,25 @@ for id in unique_remind_ids:
 	Otherwise, bidsification might not be up to date if edits are made to heuristic.py
 
 	'''
-	heudiconv_command_cleanup=f'rm -rf {bids_path}/.heudiconv/{id}'
-	heudiconv_command_loc=f'bash run_heudiconv.sh {id} loc'
-	heudiconv_command_nf=f'bash run_heudiconv.sh {id} loc'
+	# heudiconv_command_cleanup=f'rm -rf {bids_path}/.heudiconv/{id}'
+	# heudiconv_command_loc=f'bash run_heudiconv.sh {id} loc'
+	# heudiconv_command_nf=f'bash run_heudiconv.sh {id} loc'
 
-	if os.path.isdir(f'bids_path/sub-{id}'):
-		print(f'BIDSified data already exists for {id}')
-	else:
-		print(heudiconv_command_cleanup)
-		print(heudiconv_command_loc)
-		print(heudiconv_command_nf)
+	# if os.path.isdir(f'{bids_path}/sub-{id}'):
+	# 	log_and_print(f'BIDSified data already exists for {id}')
+	# else:
+	# 	log_and_print(f'Running heudiconv to bidsify data for {id}')
+	# 	os.system(heudiconv_command_cleanup)
+	# 	os.system(heudiconv_command_loc)
+	# 	os.system(heudiconv_command_nf)
 
+
+# loop through ids to prep dicoms / bidsfy
+for id in unique_remind_ids:
+	try:
+		run_pipeline_one_participant(id)
+	except Exception as e:
+		log_and_print(f'ERROR, {e}')
 
 
 
