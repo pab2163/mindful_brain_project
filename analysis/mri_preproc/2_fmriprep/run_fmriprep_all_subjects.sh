7175bae3878c47a864dd79557fa4e8c678cebfd0
Mexico <mexico@Mexicos-MacBook-Air.local>
2025-02-14 11:37:25 -0500

    set up fmriprep pipeline for ec2

---
+++ analysis/mri_preproc/2_fmriprep/run_fmriprep_all_subjects.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# No participant specified here, so it will try to run on all subjects serially
+
+docker run -tid --rm \
+    -v /neurodata/mindful_brain_project/data/bids_data/:/data:ro \
+    -v /neurodata/mindful_brain_project/data/fmriprep/:/out \
+    -v /neurodata/license.txt:/fslicense.txt:ro \
+    -v /neurodata/work:/work \
+    nipreps/fmriprep:23.2.1 \
+    /data /out/fmriprep-23.2.1 \
+    participant \
+    --write-graph \
+    -w /work --fs-license-file /fslicense.txt
