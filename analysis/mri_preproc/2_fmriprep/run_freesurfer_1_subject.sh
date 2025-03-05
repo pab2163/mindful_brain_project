0453baa0923bbc308f15b5b7cbba9142cd0e51c4
Paul Bloom <bloompa@neurofeedbackEc2.nyspi.local>
2025-03-05 15:01:28 -0500

    clean up freesurfer run script

--- analysis/mri_preproc/2_fmriprep/run_freesurfer_1_subject.sh
+++ analysis/mri_preproc/2_fmriprep/run_freesurfer_1_subject.sh
@@ -9,12 +9,22 @@ fi
 # Assign the input argument to a variable
 input_subject=$1
 
+# Set FreeSurfer paths
+export FS_SUBJECT_DIR_LOCAL=/neurodata/mindful_brain_project/data/freesurfer/
+export LICENSE_FILE=/neurodata/license.txt
+export WORK_DIR=/neurodata/work
+export INPUT_DIR=/neurodata/mindful_brain_project/data/bids_data/${input_subject}/ses-loc/anat/
+export FS_IMAGE=freesurfer/freesurfer:7.2.0
+
+# Run FreeSurfer inside Docker
 docker run -tid \
-    -v /neurodata/mindful_brain_project/data/bids_data/:/data:ro \
-    -v /neurodata/mindful_brain_project/data/freesurfer/:/out \
-    -v /neurodata/license.txt:/fslicense.txt:ro \
-    -v /neurodata/work:/work \
-    freesurfer/freesurfer:23.2.1 \
-        recon-all -autorecon1 \
-        -wsthresh 30 \
-        -s $input_subject
+    -v "$FS_SUBJECT_DIR_LOCAL":/usr/local/freesurfer/subjects/ \
+    -v /neurodata/license.txt:/license.txt:ro \
+    -e FS_LICENSE='/license.txt' \
+    -v "$WORK_DIR":/work \
+    -v "$INPUT_DIR":/input \
+    -e SUBJECTS_DIR=/usr/local/freesurfer/subjects/ \
+    $FS_IMAGE \
+    recon-all -autorecon-all \
+        -s "$input_subject" \
+        -i /input/"${input_subject}"_ses-loc_T1w.nii.gz
