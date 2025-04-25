13f6f0b0810c13c35ef999c626017cf982dd6a3b
Mexico <mexico@Mexicos-MacBook-Air.local>
2025-04-25 17:02:17 -0400

    add fmriprep loop no scd

---
+++ analysis/mri_preproc/2_fmriprep/run_fmriprep_loop_no_sdc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+# PARTICIPANTS=("sub-remind2002", "sub-remind2018", "sub-remind2021",
+#               "sub-remind2037", "sub-remind2055", "sub-remind2058", 
+#               "sub-remind2059", "sub-remind2071", "sub-remind3007", 
+#               "sub-remind3010")
+
+PARTICIPANTS=('fake', 'fake2', 'fake3')
+
+for PARTICIPANT in "${PARTICIPANTS[@]}"; do
+    docker run -tid \
+        -v /neurodata/mindful_brain_project/data/bids_data/:/data:ro \
+        -v /neurodata/mindful_brain_project/data/fmriprep/:/out \
+        -v /neurodata//neurodata/mindful_brain_project/data/fmriprep/fmriprep-23.2.1/sourcedata/freesurfer:/freesurfer \
+        -v /neurodata/license.txt:/fslicense.txt:ro \
+        -v /neurodata/work:/work \
+        nipreps/fmriprep:23.2.1 \
+        /data /out/fmriprep-23.2.1-ignorefieldmaps \
+        participant \
+        --participant-label "$input_subject" \
+        -w /work --fs-license-file /fslicense.txt \
+        --fs-no-reconall  \
+        --fs-subjects-dir /freesurfer 
+        --ignore fieldmaps \
+    docker wait 
+done
