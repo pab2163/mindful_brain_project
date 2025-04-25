534b631467ca435d975c1d00b4b0a50e71f44722
Mexico <mexico@Mexicos-MacBook-Air.local>
2025-04-25 17:23:22 -0400

    run fmriprep loop

--- analysis/mri_preproc/2_fmriprep/run_fmriprep_loop_no_sdc.sh
+++ analysis/mri_preproc/2_fmriprep/run_fmriprep_loop_no_sdc.sh
@@ -11,13 +11,13 @@ for PARTICIPANT in "${PARTICIPANTS[@]}"; do
     docker run --rm -it \
         -v /neurodata/mindful_brain_project/data/bids_data/:/data:ro \
         -v /neurodata/mindful_brain_project/data/fmriprep/:/out \
-        -v /neurodata//neurodata/mindful_brain_project/data/fmriprep/fmriprep-23.2.1/sourcedata/freesurfer:/freesurfer \
+        -v /neurodata/mindful_brain_project/data/fmriprep/fmriprep-23.2.1/sourcedata/freesurfer:/freesurfer \
         -v /neurodata/license.txt:/fslicense.txt:ro \
         -v /neurodata/work:/work \
         nipreps/fmriprep:23.2.1 \
         /data /out/fmriprep-23.2.1-ignorefieldmaps \
         participant \
-        --participant-label "$input_subject" \
+        --participant-label "$PARTICIPANT" \
         -w /work --fs-license-file /fslicense.txt \
         --fs-no-reconall  \
         --fs-subjects-dir /freesurfer \
